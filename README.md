de330f90bb3aa430f00d3115eb564da2da8f1831
Macovei Tudor <tudor_macovei@iCloud.com>
2019-01-12 18:12:06 +0200

    Add player sprite and movement

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flying-prototype
 A score attack shoot 'em up made in Unity.
+
+# TODO
+1. Force Field
+2. Enemies
+3. Animations / Graphics
+4. Sounds
+5. Music
---
+++ flying-prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D PlayerObj;
+
+    private bool rotateCW;
+    private bool rotateCCW;
+    private bool accelerate;
+
+    public int AccelerationPower { get; set; }
+    public int RotationPower { get; set; }
+
+    // Use this for initialization
+    void Start ()
+    {
+        AccelerationPower = 500;
+        RotationPower = 400;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+	}
+
+    // Used for physics operations
+    void FixedUpdate()
+    {
+        Vector2 forwardVector = Vector2FromAngle(PlayerObj.rotation);
+
+        if (accelerate && PlayerObj.velocity.magnitude < 5)
+        {
+            PlayerObj.AddForce(forwardVector * AccelerationPower * Time.deltaTime, ForceMode2D.Force);
+            RotationPower = 100;
+        }
+        if (rotateCW)
+        {
+            PlayerObj.rotation += RotationPower * Time.deltaTime;
+            Debug.Log(RotationPower);
+        }
+        if (rotateCCW)
+        {
+            PlayerObj.rotation -= RotationPower * Time.deltaTime;
+        }
+        RotationPower = 400;
+    }
+
+    Vector2 Vector2FromAngle(float a)
+    {
+        a = a * Mathf.Deg2Rad;
+        return new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+    }
+
+    void GetInput()
+    {
+        rotateCCW = false;
+        rotateCW = false;
+        accelerate = false;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            accelerate = true;
+        if (Input.GetKey(KeyCode.RightArrow))
+            rotateCW = true;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rotateCCW = true;
+    }
+}
